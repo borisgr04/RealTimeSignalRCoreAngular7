78d2340074622a2a89580c2a7c186ef1554d931d
Boris Arturo González Rivera <borisgr04@gmail.com>
2019-04-07 22:36:27 -0500

    first commit

---
+++ WebRealTime/Controllers/ChartController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebRealTime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private IHubContext<ChartHub> _hub;
+
+        public ChartController(IHubContext<ChartHub> hub)
+        {
+            _hub = hub;
+        }
+
+        public IActionResult Get()
+        {
+            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
---
+++ WebRealTime/HubConfig/ChartHub.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRealTime
+{
+    public class ChartHub:Hub
+    {
+        public async Task BroadcastChartData(List<ChartModel> data) => await Clients.All.SendAsync("broadcastchartdata", data);
+
+    }
+}
